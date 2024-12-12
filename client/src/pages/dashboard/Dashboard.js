2659c811e49e943e97059e03d0b269e8c2c675c0
Ragab <89777423+laca8@users.noreply.github.com>
2024-12-12 15:04:43 +0200

    Update Dashboard.js

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -47,7 +47,7 @@ const Dashboard = () => {
         <Error error={error} />
       ) : (
         <>
-        <Alert severity="info" dir='rtl' style={{ marginTop: "10px" }}>
+        <Alert severity="info" dir='rtl' style={{ marginTop: "10px" ,fontSize:'20px',fontWeight:"bold"}}>
   <AlertTitle>ملاحظة</AlertTitle>
   يمكنك تحديد اي فئة تريد ان تراها في الرسومات البيانية عن طريقة الضغط علي الفئات الاخري
 </Alert>
