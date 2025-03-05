10ded8c1eff907509cd66529e37959694aeab212
laca8 <ahmedragab01140331005@gmail.com>
2025-03-05 19:26:39 +0200

    upload

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      //"mongodb+srv://laca:jae09908@cluster0.gjxhg.mongodb.net/egypt?retryWrites=true&w=majority"
       "mongodb+srv://ahmed:VernA2525@cluster0.2qrrr0b.mongodb.net/egypt?retryWrites=true&w=majority"
     );
     console.log("db connect....");
